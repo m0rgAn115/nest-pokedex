7be9b4a34e00b7bc7a36a02997f8b14198974aa0
Angel Mondragon <damianmor3005@hotmail.com>
2025-03-01 23:07:06 -0600

    Implementacion de poblacion automatica

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
+  constructor(
+    private readonly pokemonService: PokemonService,
+
+    private readonly http: AxiosAdapter,
+  
+  ) {}
+
 
   async executeSeed() {
-    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=50')
+
+    const insertPromisesArray = []
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no:number = +segments[segments.length - 2]
+
       console.log(name, no);
+      const pokemon:CreatePokemonDto = {
+        name,
+        no
+      }
+      this.pokemonService.create(pokemon)
       
     })
     return data.results;
